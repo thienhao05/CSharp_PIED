6dbf29b9d6c28b0870f6677390ad0ff41c38f18c
Thien Hao <93728241+thienhao05@users.noreply.github.com>
2026-01-18 15:59:01 +0700

    Constructor Overloading l demo

--- CSharp.Lession2/Program.cs
+++ CSharp.Lession2/Program.cs
@@ -6,40 +6,31 @@ class Program
 
     class Student
     {
-       // Instance Variables - Biến dùng để lưu trữ INFO của 1 Object
-       // Age, Name, Gender
-       
-       // Trong C# thuật ngữ  Instance Variables 
-            // - Được gọi là Backed-Field, Data Field
-            // - Biến dùng để lưu INFO của 1 Object
-        // Nếu mà mình ko ghi public thì mặc định sẽ là private
-        // Access Modifier: public - private - protected - default
-        // string _id = "36";
-        // int b = 36;
-        // double c = 3.6;
-        
-        // Nếu mà mình ko bỏ giá trị khởi tạo, thì sẽ được mang các giá trị mặc định
-        // int : 0
-        // bool : false
-        // Datetime: 01/01/0001 00:00:00
-        // string : null
-        // class : null
-        // array : null
-        // struct : 0
-        
-        // Lưu ý: cho mình dễ nhớ
-        // Value Type: 0
-        // Reference Type: null (ko có vùng nhớ)
-        
-        // >_< trả lời string: "", struct: null là sai !!!
+       // Constructor - hàm khởi tạo, phễu
+       // 1 Class có thể có nhiều constructor đc không ? Yes I do
+       // 1 Class thì ko có 1 đoạn code nào về constructor đc không | Yes I do
+            // 1 Empty Constructor sẽ được khởi tạo mặc định, ngay cả khi chúng ta không khai báo luôn
+        // Khi có nhiều constructor trong class thì nó được gọi là - Constructor Overloading
         public string name;
-        private int age;
+        public int age;
+        
+        public Student(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        public Student()
+        {
+            this.name = "Hào";
+            this.age = 20;
+        }
+
     }
     
 
     static void Main(string[] args)
     {
-        Student s1 = new Student();
-        Console.WriteLine("Name: " + s1.name); //null nên ko in ra, chứ ko phải chuỗi rỗng nha
+       
     }
 }
